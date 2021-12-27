808ba97bd14556b2b99b4d0b836d2ba0c533280b
ItsAdityaKSingh <adityakksingh23@gmail.com>
2021-12-27 12:24:22 +0530

    iOS generated files

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/adityakumarsingh/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/adityakumarsingh/Repositories/Deluge-mobile-remote-client"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.4"
+export "FLUTTER_BUILD_NUMBER=15"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
